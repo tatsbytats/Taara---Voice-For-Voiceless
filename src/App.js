3073be7929a9241ec681c193070d396567043503
Justine <justine.llona14@gmail.com>
2025-05-08 02:30:55 +0800

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // import LoginForm from './components/common/LoginForm';
 import SignUpForm from './components/common/SignUpForm';
 import AdminDashboard from './pages/admin/AdminDashboard';
@@ -17,8 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           {/* <Route path="/login" element={<LoginForm />} />        */}
-          <Route path="/signup" element={<SignUpForm />} />     
-          <Route path="/dashboard" element={<AdminDashboard />} /> 
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/login" element={<AdminLoginForm />} />
           <Route path="/fundraising" element={<FundraiserCard />} />
           <Route path="/rescue" element={<RescueTab />} />
